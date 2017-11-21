ae48440a528cae5e9d13b11450891f3a60319fe9
cob999 <cobrien@cloudmaster.com>
2017-11-21 11:54:30 +0000

    add some sets

---
+++ Flattener.Core/Set.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Flattener.Core
+{
+    public class Set<T>
+    {
+        private T[] _backingArray;
+
+        private const int IncrementSize = 5;
+
+        public Set()
+        {
+            _backingArray = new T[IncrementSize];
+        }
+
+        public int Count { get; private set; }
+
+        public void Add(T value)
+        {
+            if (!Contains(value))
+            {
+                if (Count == _backingArray.Length)
+                {
+                    ExpandArray();
+                }
+                _backingArray[Count++] = value;
+            }
+        }
+
+        public void Remove(T value)
+        {
+            if (Contains(value))
+            {
+            }
+        }
+
+        private void ExpandArray()
+        {
+            var oldSize = _backingArray.Length;
+            var newSize = oldSize + 5;
+
+            Array.Resize(ref _backingArray, newSize);
+        }
+
+        public bool Contains(T value)
+        {
+            foreach (var item in _backingArray)
+            {
+                if (item.Equals(value))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Flattener.Tests/SetTests.cs
@@ -0,0 +1,59 @@
+﻿using Flattener.Core;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Flattener.Tests
+{
+    [TestFixture]
+    public class SetTests
+    {
+        [Test]
+        public void CanWeResize()
+        {
+            var sut = new Set<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                sut.Add(i);
+            }
+        }
+
+        [Test]
+        public void Contains_WithEmptySet_ReturnsFalse()
+        {
+            var sut = new Set<int>();
+
+            sut.Contains(1).ShouldBeFalse();
+        }
+
+        [TestCase(1, true)]
+        [TestCase(2, false)]
+        public void Contains_WithOneValueSet_ReturnsExpected(int matchValue, bool expected)
+        {
+            var sut = new Set<int>();
+
+            sut.Add(1);
+
+            sut.Contains(matchValue).ShouldBe(expected);
+        }
+
+        [Test]
+        public void Count_WithEmptySet_ReturnsZero()
+        {
+            var sut = new Set<int>();
+
+            sut.Count.ShouldBe(0);
+        }
+
+        [Test]
+        public void Count_AfterAdd_ReturnsExpected()
+        {
+            var sut = new Set<int>();
+
+            sut.Add(1);
+
+            sut.Count.ShouldBe(1);
+        }
+
+    }
+}
